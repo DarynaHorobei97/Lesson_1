3373a6bd29ffd927f884e7e1f2d4d11493dd007d
DarynaHorobei97 <daryna_horobei1@ukr.net>
2023-01-22 21:08:29 +0200

    л

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static int year = 2023;
+
+    public static int sumOfTwoNumbers(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sumOfTwoNumbers(5, 12));
+        System.out.println(year);
+    }
+}
